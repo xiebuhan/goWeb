672910dacae5adbac85e80d389e1ca58504a5dcd
xu kaibo <875471384@qq.com>
2023-05-04 20:04:46 +0800

    集成 gorilla mux 路由

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module goWeb
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -2,26 +2,56 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func handlefunc(w http.ResponseWriter, r *http.Request)  {
-	w.Header().Set("Content-Type","text/html;charset=utf-8")
-	fmt.Fprint(w,"请求路径为:" + r.URL.Path)
-	if r.URL.Path == "/"{
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w,"此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-			"<a href=\"www.baidu.com\">baiduddd</a>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
-			"<p>如有疑惑，请联系我们。</p>")
-	}
-
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
+}
+
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprint(w, "文章 ID："+id)
+}
+
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "访问文章列表")
+}
+
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "创建新的文章")
+}
+
+
 func main() {
-	http.HandleFunc("/", handlefunc)
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+	router.HandleFunc("/About",aboutHandler).Methods("Get").Name("about")
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+
+	//自定义404页面
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+
+
+
 	http.ListenAndServe(":3000", nil)
 }
