04a5ac80d05c676e39d1ca971c282c0da76e6672
xu kaibo <875471384@qq.com>
2023-05-04 19:47:39 +0800

    404状态码

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func handlefunc(w http.ResponseWriter, r *http.Request)  {
 		fmt.Fprint(w,"此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 			"<a href=\"www.baidu.com\">baiduddd</a>")
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
 			"<p>如有疑惑，请联系我们。</p>")
 	}
