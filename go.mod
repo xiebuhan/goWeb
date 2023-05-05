31e3c5f1092a3748a656097e62e8d3d7219b750e
xu kaibo <875471384@qq.com>
2023-05-05 18:01:31 +0800

    连接数据库

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module goWeb
 
 go 1.19
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,55 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/go-sql-driver/mysql"
 	"html/template"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 	"unicode/utf8"
 
 	"github.com/gorilla/mux"
 )
+var db *sql.DB
+
+func initDB()  {
+	var err error
+	config := mysql.Config{
+		User:                 "homestead",
+		Passwd:               "secret",
+		Addr:                 "127.0.0.1:3306",
+		Net:                  "tcp",
+		DBName:               "goblog",
+		AllowNativePasswords: true,
+	}
+	// 准备数据库连接池
+	db,err :=  sql.Open("mysql",config.FormatDSN())
+	checkError(err)
+
+	// 设置最大连接数
+	db.SetMaxOpenConns(25)
+	// 设置最大空闲连接数
+	db.SetMaxIdleConns(25)
+	// 设置每个链接的过期时间
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	//尝试连接，失败报错
+	err = db.Ping()
+	checkError(err)
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
 type ArticlesFormData struct {
 	Title, Body string
 	URL         *url.URL
