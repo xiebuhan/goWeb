ae18e0118a0b82f77d19a9a495c7aaed91177e93
xu kaibo <875471384@qq.com>
2023-05-08 19:46:26 +0800

    更新文章

--- main.go
+++ main.go
@@ -90,16 +90,13 @@ type Article struct {
 
 
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
 
+   id := getRouteVariable("id",r)
 	// 2 读取对应的文章数据
-	article := Article{}
-	query :="select * from articles where id = ?"
-	err := db.QueryRow(query,id).Scan(&article.ID, &article.Title, &article.Body)
+	article,err := getArticlesByID(id)
 
 	if err != nil {
-		if err == sql.ErrNoRows{
+		if err == sql.ErrNoRows {
 			//3.1 数据未找到
 			w.WriteHeader(http.StatusNotFound)
 			fmt.Fprint(w,"404数据未找到")
@@ -128,21 +125,7 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.PostFormValue("title")
 	body := r.PostFormValue("body")
 
-	errors := make(map[string]string)
-
-	//验证标题
-	if title == "" {
-		errors["title"] = "标题不能为空"
-	} else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(title) > 40 {
-		errors["title"] = "标题长度需要介于3和40"
-	}
-
-	//验证内容
-	if title == "" {
-		errors["body"] = "内容不能为空"
-	} else if utf8.RuneCountInString(body) < 10 {
-		errors["body"] = "内容"
-	}
+    errors:= ValidateArticleFormData(title,body)
 
 	// 检查是否有错误
 	if len(errors) == 0 {
@@ -273,12 +256,144 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 //编辑文章
 func articlesEditHandler(w http.ResponseWriter,r *http.Request)  {
 	//获取url参数
-	vars := mux.Vars(r)
-	id := vars["id"]
-
+	id := getRouteVariable("id",r)
 	//2 读取对应的文章数据
+	article,err := getArticlesByID(id)
+	if err != nil {
+		if err == sql.ErrNoRows{
+			//3.1 数据未找到
+			w.WriteHeader(http.StatusFound)
+			fmt.Fprint(w,"404文章未找到")
+		} else {
+			checkError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w,"500 服务器内部错误")
+		}
+	} else {
+		// 4 读取成功 显示表单
+		updateURL,_:= router.Get("articles.update").URL("id",id)
+		data := ArticlesFormData{
+			Title:  article.Title,
+			Body:   article.Body,
+			URL:    updateURL,
+			Errors: nil,
+		}
+
+		tmpl,err := template.ParseFiles("resources/views/articles/edit.gohtml")
+		checkError(err)
+
+		err = tmpl.Execute(w,data)
+		checkError(err)
+
+	}
+
+
+
 
 }
+//更新数据
+func articlesUpdateHandler(w http.ResponseWriter,r *http.Request){
+	// 1 获取RUL参数
+	id := getRouteVariable("id",r)
+	// 2 读取对应文章的数据
+	_,err:=getArticlesByID(id)
+
+	//3 如果出现错误
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusFound)
+			fmt.Fprint(w,"文章没有找到")
+		}else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w,"505 系统错误")
+		}
+
+	} else {
+		// 4 未出现错误
+
+		// 4.1 表单验证
+		title := r.PostFormValue("title")
+		body := r.PostFormValue("body")
+
+		errors := ValidateArticleFormData(title, body)
+
+		if len(errors) == 0{
+			// 表单验证通过，更新数据
+			query := "update articles set title = ?,body = ? where id = ?"
+			rs,err := db.Exec(query,title,body,id)
+			if err != nil {
+				checkError(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprint(w,"500服务器内部错误")
+			}
+
+			 // 更新成功，跳转到文章详情页
+			if n,_ := rs.RowsAffected(); n>0 {
+			 showURL,_ := router.Get("articles.show").URL("id",id)
+			 http.Redirect(w,r,showURL.String(),http.StatusFound)
+			} else {
+				fmt.Fprint(w,"您没有做任何更改")
+			}
+
+
+
+
+		}else {
+			// 表单没有通过 显示原因
+			updateUrl,_ :=  router.Get("articles.update").URL("id",id)
+			data := ArticlesFormData{
+				Title:  title,
+				Body:   body,
+				URL:    updateUrl,
+				Errors: errors,
+			}
+
+			tmpl,err := template.ParseFiles("resources/views/articles/edit.gohtml")
+			checkError(err)
+			err = tmpl.Execute(w,data)
+			checkError(err)
+
+		}
+
+	}
+}
+
+//获取url
+func getRouteVariable(parameterName string,r *http.Request) string  {
+	vars := mux.Vars(r)
+	return  vars[parameterName]
+}
+
+// 获取文章ID
+func getArticlesByID(id string)(Article,error)  {
+	article := Article{}
+	query := "select * from articles where id = ?"
+	err := db.QueryRow(query,id).Scan(&article.ID,&article.Title,&article.Body)
+	return article,err
+
+}
+
+func ValidateArticleFormData(title string,body string) map[string]string{
+	errors := make(map[string]string)
+	// 验证标题
+	if title == "" {
+		errors["title"] = "标题不能为空"
+	} else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(title) > 40 {
+		errors["title"] = "标题长度需介于 3-40"
+	}
+
+	// 验证内容
+	if body == "" {
+		errors["body"] = "内容不能为空"
+	} else if utf8.RuneCountInString(body) < 10 {
+		errors["body"] = "内容长度需大于或等于 10 个字节"
+	}
+
+	return errors
+
+}
+
+
 
 func main() {
 	initDB()
